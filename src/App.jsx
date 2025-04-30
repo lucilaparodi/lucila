6f9acad78db81b36b891d9c5b3b7a18bacf2c025
lucilaparodi <lucila.parodi.c@gmail.com>
2025-04-30 00:02:36 -0700

    initialize React project with Vite setup and basic configuration

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+        }}
+      >
+        <h1>Lucila</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
