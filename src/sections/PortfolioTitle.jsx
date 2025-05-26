3abfa72a6b7566321be8f8aa861d6fe6be5ddddf
lucilaparodi <lucila.parodi.c@gmail.com>
2025-05-26 00:49:40 -0700

    fix import path for sombraGrupo asset in PortfolioTitle component

--- src/sections/PortfolioTitle.jsx
+++ src/sections/PortfolioTitle.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import sombraGrupo from "../assets/sombraGrupo.webp";
+import sombraGrupo from "../assets/SombraGrupo.webp";
 
 /**
  * PortfolioTitle
